1bed0db652c11b1f97aa87602697ad02eba63423
taeseong <rlaxotjd12@naver.com>
2020-03-14 18:52:11 +0900

    first commit

---
+++ src/main/java/com/example/elasticsearch/book/service/BookService.java
@@ -0,0 +1,24 @@
+package com.example.elasticsearch.book.service;
+
+import com.example.elasticsearch.book.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
+public interface BookService {
+
+    Book save(Book book);
+
+    void delete(Book book);
+
+    Book findOne(String id);
+
+    Iterable<Book> findAll();
+
+    Page<Book> findByAuthor(String author, PageRequest pageRequest);
+
+    List<Book> findByTitle(String title);
+
+
+}
---
+++ src/main/java/com/example/elasticsearch/book/service/BookServiceImpl.java
@@ -0,0 +1,48 @@
+package com.example.elasticsearch.book.service;
+
+import com.example.elasticsearch.book.model.Book;
+import com.example.elasticsearch.book.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public void delete(Book book) {
+        bookRepository.delete(book);
+    }
+
+    @Override
+    public Book findOne(String id) {
+        return bookRepository.findOne(id);
+    }
+
+    @Override
+    public Iterable<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Page<Book> findByAuthor(String author, PageRequest pageRequest) {
+        return bookRepository.findByAuthor(author, pageRequest);
+    }
+
+    @Override
+    public List<Book> findByTitle(String title) {
+        return bookRepository.findByTitle(title);
+    }
+}
